b0b359174ff4b94689b60a8f09a478c49da41078
Foyez <foyezar@gmail.com>
2018-11-20 21:38:31 +0600

    Database repair

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express  = require('express'),
-      app      = express(),
-      { Todo } = require('./models/todo')
+const express      = require('express'),
+      { mongoose } = require('./db/mongoose'),
+      { Todo }     = require('./models/todo')
 
+const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
@@ -22,6 +23,12 @@ app.post('/todos', (req, res) => {
   }).catch(e => {
     res.status(400).send(e)
   })
+})
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({ todos })
+  }).catch(e => res.status(400).send(e))
 })
 
 app.listen(port, () => console.log(`Server started on ${port}`));
--- db/mongoose.js
+++ db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 // mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 // mongodb://<dbuser>:<dbpassword>@ds113134.mlab.com:13134/node-todo-api
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/DeployApp', { useNewUrlParser: true })
 
 module.exports = { mongoose }
